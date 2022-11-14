62250f8abf0da95112626c278d90ed44ef1cb667
JAMESBRADLEY99 <jab.bradley99@gmail.com>
2022-11-14 15:35:41 +0000

    get reveiws

--- Controllers/controllers.js
+++ Controllers/controllers.js
@@ -1,9 +1,17 @@
-const { selectCategories } = require('../Models/models.js');
+const { selectCategories, selectReviews } = require('../Models/models.js');
 
 exports.getCategories = (req, res) => {
     
     selectCategories()
     .then((categories) => {
         return res.send(categories)
+    })
+}
+
+exports.getReviews = (req, res) => {
+    selectReviews()
+    .then((reviews) => {
+        console.log(reviews)
+        return res.send(reviews)
     })
 }
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { getCategories } = require('./Controllers/controllers.js')
+const { getCategories, getReviews } = require('./Controllers/controllers.js')
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/api/categories', getCategories)
+
+app.get('/api/reviews', getReviews)
 
 app.all("/*", (req, res) => {
     res.status(404).send({ msg: "Ooops: Noting to see here." });
